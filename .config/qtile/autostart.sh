f976557363da51e13268d12b194b3278d7eb563d
ton1czech <tonyasek007@gmail.com>
2022-12-01 21:25:23 +0100

    👌 IMPROVE: autostart

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -7,29 +7,12 @@ function run {
   fi
 }
 
-
-
 #starting utility applications at boot time
 lxsession &
-run nm-applet &
-run pamac-tray &
-#numlockx on &
-blueman-applet &
-#flameshot &
+xrandr --output DVI-D-0 --mode 1920x1080 -r 144 &
+xrandr --output DP-0 --mode 2560x1440 --primary -r 144 &
+nitrogen --restore
 picom --config $HOME/.config/picom/picom.conf --experimental-backends &
-#/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-dunst &
-feh --bg-fill /usr/share/wallpapers/garuda-wallpapers/ken-yeung-VnSjwrAy1PQ-unsplash.jpg
-#starting user applications at boot time
+run nm-applet &
 run volumeicon &
-#run discord &
-#nitrogen --random --set-zoom-fill &
-#run caffeine -a &
-#run vivaldi-stable &
-#run firefox &
-#run thunar &
-#run dropbox &
-#run insync start &
-#run spotify &
-#run atom &
-#run telegram-desktop &
+dunst &
