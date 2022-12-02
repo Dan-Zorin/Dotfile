b1af85899d548befe1ba0eab9c95c0370871c11b
ton1czech <tonyasek007@gmail.com>
2022-12-02 16:49:11 +0100

    🎁 NEW: reload qtile after start

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -11,6 +11,7 @@ function run {
 lxsession &
 xrandr --output DVI-D-0 --mode 1920x1080 -r 144 &
 xrandr --output DP-0 --mode 2560x1440 --primary -r 144 &
+qtile cmd-obj -o cmd -f restart
 nitrogen --restore
 picom --config $HOME/.config/picom/picom.conf --experimental-backends &
 run nm-applet &
