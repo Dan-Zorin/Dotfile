2e314a52c584388b127743f710f4063a527df94f
ton1czech <tonyasek007@gmail.com>
2022-12-04 20:37:23 +0100

    🐛 BUG FIX: using wrong column

--- dmenuscripts/quickmarks.sh
+++ dmenuscripts/quickmarks.sh
@@ -2,9 +2,9 @@
 
 readarray -t quickmarks < "$HOME/dmenuscripts/quickmarks.txt"
 
-quickmarkslist=$(printf "%s\n" "${quickmarks[@]}" | awk '{print $1"   |   "$3}' | sort)
+quickmarkslist=$(printf "%s\n" "${quickmarks[@]}" | awk '{print $1"   |   "$2}' | sort)
 
 choice=$(printf "%s\n" "$quickmarkslist" | dmenu -l 10 -fn "Satoshi:semibold:size=13" -p "quickmarks:") "$@" || exit
 
-url=$(echo "${choice}" | awk '{print $2}') || exit
+url=$(echo "${choice}" | awk '{print $NF}') || exit
 brave "$url"
