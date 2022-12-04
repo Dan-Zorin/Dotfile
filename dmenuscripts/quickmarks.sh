473f60389e01e20df98c2257bb3c51c5996634f3
ton1czech <tonyasek007@gmail.com>
2022-12-04 20:26:38 +0100

    👌 IMPROVE: printing just a name and not full address + smaller font

--- dmenuscripts/quickmarks.sh
+++ dmenuscripts/quickmarks.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 readarray -t quickmarks < "$HOME/dmenuscripts/quickmarks.txt"
-quickmarkslist=$(printf "%s\n" "${quickmarks[@]}" | awk '{print $1" - "$NF}' | sort)
 
-choice=$(printf "%s\n" "$quickmarkslist" | dmenu -l 10 -p "quickmarks:") "$@" || exit
+quickmarkslist=$(printf "%s\n" "${quickmarks[@]}" | awk '{print $1"   |   "$3}' | sort)
 
-url=$(echo "${choice}" | awk '{print $NF}') || exit
+choice=$(printf "%s\n" "$quickmarkslist" | dmenu -l 10 -fn "Satoshi:semibold:size=13" -p "quickmarks:") "$@" || exit
+
+url=$(echo "${choice}" | awk '{print $2}') || exit
 brave "$url"
