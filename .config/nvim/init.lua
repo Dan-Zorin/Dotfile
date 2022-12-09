069de9be41293a1bdea20c6d97f70b0da81cb45a
ton1czech <tonyasek007@gmail.com>
2022-12-09 21:15:19 +0100

    🎁 NEW: plugins - commenting, surroundings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,4 +2,4 @@ require("ton1czech.plugins")
 require("ton1czech.core.options")
 require("ton1czech.core.keymaps")
 require("ton1czech.core.colorscheme")
-
+require("ton1czech.plugins.comment")
--- .config/nvim/lua/ton1czech/plugins.lua
+++ .config/nvim/lua/ton1czech/plugins.lua
@@ -1,29 +1,29 @@
 -- auto install packer if not installed
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
-    vim.cmd([[packadd packer.nvim]])
-    return true
-  end
-  return false
+    local fn = vim.fn
+    local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+    if fn.empty(fn.glob(install_path)) > 0 then
+      fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+      vim.cmd([[packadd packer.nvim]])
+      return true
+    end
+    return false
 end
 local packer_bootstrap = ensure_packer() -- true if packer was just installed
 
 -- autocommand that reloads neovim and installs/updates/removes plugins
 -- when file is saved
 vim.cmd([[ 
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins-setup.lua source <afile> | PackerSync
-  augroup end
+    augroup packer_user_config
+      autocmd!
+      autocmd BufWritePost plugins-setup.lua source <afile> | PackerSync
+    augroup end
 ]])
 
 -- import packer safely
 local status, packer = pcall(require, "packer")
 if not status then
-  return
+    return
 end
 
 -- add list of plugins to install
@@ -39,6 +39,15 @@ return packer.startup(function(use)
 
     -- maximize and restore current window split
     use("szw/vim-maximizer")
+    
+    -- add, delete, change surroundings
+    use("tpope/vim-surround")
+
+    -- replace with register contents using motion
+    use("inkarkat/vim-ReplaceWithRegister")
+
+    -- commenting with gc
+    use("numToStr/Comment.nvim")
 
     if packer_bootstrap then
         require("packer").sync()
