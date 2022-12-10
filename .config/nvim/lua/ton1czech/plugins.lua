1f9dd1b0be61cd75f180cd9a91c42abb6edacb46
ton1czech <tonyasek007@gmail.com>
2022-12-10 09:33:58 +0100

    🎁 NEW: wakatime

--- .config/nvim/lua/ton1czech/plugins.lua
+++ .config/nvim/lua/ton1czech/plugins.lua
@@ -78,6 +78,9 @@ return packer.startup(function(use)
     -- git integration
     use("lewis6991/gitsigns.nvim")    -- show line modifications
 
+    -- wakatime
+    use("wakatime/vim-wakatime")    -- wakatime integration
+
     if packer_bootstrap then
         require("packer").sync()
     end
