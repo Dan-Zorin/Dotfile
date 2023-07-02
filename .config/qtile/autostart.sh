44615695e46afe6fb0a87acf71efa0daf3c4e71d
Dan | Zorin <97075187+Dan-Zorin@users.noreply.github.com>
2023-07-02 01:48:25 -0500

    Update autostart.sh

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -9,8 +9,7 @@ function run {
 
 #starting utility applications at boot time
 lxsession &
-xrandr --output DVI-D-0 --mode 1920x1080 -r 144 &
-xrandr --output DP-0 --mode 2560x1440 --primary -r 144 &
+xrandr --output DisplayPort-0 --mode 1440x900 --primary -r 75 &
 nitrogen --restore
 picom --config $HOME/.config/picom/picom.conf --experimental-backends &
 run nm-applet &
