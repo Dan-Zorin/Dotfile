19a33fd705e577287ebbd1ffb00c73b382c65b8f
Dan | Zorin <97075187+Dan-Zorin@users.noreply.github.com>
2024-05-11 02:50:16 -0500

    Update autostart.sh

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -9,8 +9,8 @@ function run {
 
 #starting utility applications at boot time
 lxsession &
-#xrandr --output HDMI-0 --mode 1440x900 --primary -r 75 &
-feh --bg-fill .Wallpaper/neon.jpg &
+xrandr --output HDMI-0 --mode 1920x1080 --primary -r 144 &
+feh --bg-fill .Wallpaper/neon.jpg 
 picom --config $HOME/.config/picom/picom.conf --experimental-backends &
 run nm-applet &
 run volumeicon &
